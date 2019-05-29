49ba0e0e63361dc94c371278ac8ef9ce80e2ee54
Luna Luisa <sanchez.reyes.luna@gmail.com>
2019-05-29 16:24:13 -0400

    fagales example

--- analysis/Fagales_example.R
+++ analysis/Fagales_example.R
@@ -10,10 +10,10 @@ devtools::install_github("dwbapst/paleotree", ref="developmentBranch")
 library(ape)
 
 # to test with otol Fagales tree
-dq <- datelife::make_datelife_query("fagales", 
+dq <- datelife::make_datelife_query("fagales",
 	get_spp_from_taxon = TRUE)
 tree_backbone<- datelife::get_otol_synthetic_tree(dq$cleaned_names,
-	ott_ids = dq$ott_ids)
+	ott_ids = dq$ott_ids, resolve = FALSE)
 # and with Fagales PBDB taxon tree
 faData <- paleotree::getCladeTaxaPBDB("Fagales")
 # make the taxon tree
@@ -30,3 +30,7 @@ mergedTrees <- merging_trees_with_MRP(tree_backbone, tree_secondary)
 
 strictMerged <- consensus(mergedTrees, p = 1, check.labels = TRUE)
 plot(strictMerged)
+
+pdf("fagales_mrp.pdf", height = 50)
+plot(supertrees_out, cex = 0.2)
+dev.off()
