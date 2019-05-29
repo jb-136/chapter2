13aa8cc9758f35b85396ff675c9ed1babfbf7e6e
Brian O'Meara <omeara.brian@gmail.com>
2019-05-29 19:16:34 -0400

    bunch of functions with new functionality

--- R/dataFunctions.R
+++ R/dataFunctions.R
@@ -88,3 +88,101 @@ fishbase_species_query <- function (species) {
   }
   return(all.records)
 }
+
+#' Get info from Open Tree of Life
+#'
+#' @param taxon The clade to investigate
+#' @return data.frame of info about that taxon. Columns include year of the study, number of taxa in the tree from that study, the study citation, and the study DOI.
+#' @export
+#' @examples
+#' info <- get_otol("Gallus")
+#' histogram(info$year) # Years in which chicken papers in OpenTree were published
+get_otol <- function(taxon) {
+  clade.info <- rotl::tnrs_match_names(taxon)
+  clade.name <- clade.info$unique_name[1]
+  id <- clade.info$ott_id[1]
+  node.info <- rotl::tol_node_info(id)
+  relevant.studies <- rotl::studies_find_trees(property="ot:ottTaxonName", value=clade.name)
+  tree.info <- data.frame()
+  all.trees <- rotl::list_trees(relevant.studies)
+  for (study.index in sequence(nrow(relevant.studies))) {
+    study.info <- rotl::get_publication(rotl::get_study_meta(relevant.studies$study_ids[study.index]))
+    for (tree.index in sequence(length(all.trees[study.index]))) {
+      phy <- NULL
+      try(phy <- rotl::get_study_tree(study_id=relevant.studies$study_ids[study.index], tree_id = gsub('tree/', '',all.trees[[study.index]][tree.index])))
+      local.result <- NULL
+      if(!is.null(phy)) {
+        try(local.result <- data.frame(Year=relevant.studies$study_year[study.index], Ntax=ape::Ntip(phy), Pub=study.info[1], DOI=attr(study.info, "DOI")))
+      } else {
+        try(local.result <- data.frame(Year=relevant.studies$study_year[study.index], Ntax=NA, Pub=study.info[1], DOI=attr(study.info, "DOI")))
+      }
+      if(!is.null(local.result)) {
+        if(nrow(tree.info)==0) {
+          tree.info <- local.result
+        } else {
+          tree.info <- rbind(tree.info, local.result)
+        }
+      }
+
+    }
+  }
+  return(tree.info)
+}
+
+#' Information about the number of species in the clade in genbank
+#'
+#' @param taxon The clade of interest
+#' @return The count of species in genbank
+#' @export
+#' @examples
+#' taxon <- "Myrmecocystus"
+#' print(paste("There are", get_genbank_count(taxon), "species of", taxon, "in GenBank"))
+get_genbank_count <- function(taxon) {
+  clade.name<- rotl::tnrs_match_names(taxon)$unique_name[1]
+  genbank.species.query <- paste0(clade.name, '[subtree] AND species[rank] AND specified[prop]')
+  genbank.species.count <-  rentrez::entrez_search(db="taxonomy", genbank.species.query, use_history=TRUE)$count
+}
+
+#' Get the count of number of sequences for different genes for this taxon
+#'
+#' This will use a few popular genes by default, but you can pass your own instead.
+#'
+#' @param taxon The clade of interest
+#' @param focal.genes Vector of gene names
+#' @return vector of counts by gene, with label being the given gene name
+#' @examples
+#' get_genbank_count_by_gene("Myrmecocystus")
+get_genbank_count_by_gene <- function(taxon, focal.genes=c("COI", "18S", "28S", "matk", "rbcl")) {
+  clade.name<- rotl::tnrs_match_names(taxon)$unique_name[1]
+  GetNucCount <- function(gene, taxon=clade.name) {
+    gene.query <- paste0(taxon, '[organism] AND ',gene)
+    Sys.sleep(3) #just to make sure we stay nice
+    return(rentrez::entrez_search(db="nuccore", gene.query, use_history=TRUE)$count)
+  }
+  gene.count <- sapply(focal.genes, GetNucCount, taxon=clade.name) #make sure not to do mclapply or similar lest you
+  return(gene.count)
+}
+
+#' Get information on the clade from pubmed
+#'
+#' This will give the total number of pubmed articles mentioning the taxon name and other search string and information on the most recent such papers.
+#'
+#' @param taxon The clade of interest
+#' @param search.string Include spaces and AND and similar search elements
+#' @param retmax how many papers to return
+#' @return List with a count element (integer) and a recent.papers element (data.frame)
+#' @export
+#' @examples
+#' taxon <- "Formicidae"
+#' results <- get_pubmed(taxon)
+#' print(paste("There are", results$count, "papers on", taxon, "and phylogeny"))
+#' print(results$recent.papers)
+
+get_pubmed <- function(taxon, search.string=' AND phylogeny',retmax=50) {
+  clade.name<- rotl::tnrs_match_names(taxon)$unique_name[1]
+  pubmed.query <- paste0(clade.name, search.string)
+  pubmed.result <- rentrez::entrez_search(db="pubmed", pubmed.query, use_history=TRUE, retmax=retmax)
+  pubmed.summaries <- rentrez::entrez_summary(db="pubmed", id=pubmed.result$id)
+  pubmed.df <- data.frame(Date=rentrez::extract_from_esummary(pubmed.summaries, elements=c("sortpubdate")), FirstAuthor=rentrez::extract_from_esummary(pubmed.summaries, elements=c("sortfirstauthor")), Journal=rentrez::extract_from_esummary(pubmed.summaries, elements=c("fulljournalname")), Title=rentrez::extract_from_esummary(pubmed.summaries, elements=c("title")), row.names=NULL)
+  return(list(count=pubmed.result$count,   recent.papers =   pubmed.df ))
+}
